be990d9ef570ca2249f24f82ae9c3d58095b6868
Student <student@Makerss-MacBook-Pro.local>
2019-03-06 11:45:52 +0000

    Passes date argument test

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,3 +1,7 @@
 def add_name(name)
   return name
 end
+
+def add_date(date)
+  return date
+end
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -2,6 +2,9 @@ require 'birthday_list'
 
 describe "Add name method" do
     it "gives list of names" do
-        expect(add_name("Joe")).to eq "Joe"
+        expect(add_name('Joe')).to eq 'Joe'
+    end
+    it "gives list of dates" do
+        expect(add_date('01/01/1990')). to eq '01/01/1990'
     end
 end
